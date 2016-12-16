caa0a1ff5b8edb5bfb457d143405592e7cdf4e13
Florin Costa <florin.costa83@gmail.com>
2016-12-16 22:14:32 +0200

    integrated with raspihats node.js module

--- raspihats.js
+++ raspihats.js
@@ -1,9 +1,23 @@
-
-// If you use this as a template, update the copyright with your own name.
-
 module.exports = function(RED) {
     "use strict";
-    //var foo = require("foo-library");
+    var raspihats = require('raspihats');
+    
+    function getBoard(boardConfig) {
+      var token = "I2C-HAT";
+      if(boardConfig.name.includes(token)) {
+        var name = boardConfig.name.replace(token, "").trim();
+        var board = new raspihats.i2cHats[name](parseInt(boardConfig.address, 16));
+        //console.log("#" + board.getName() + "#" + " " + typeof(board.getName()));
+        //console.log("#" + boardConfig.name + "#" + " " + typeof(boardConfig.name));
+        console.log(board.getName().length);
+        console.log(boardConfig.name.length);
+        if(board.getName().includes(boardConfig.name)) {
+          return board;
+        }
+        throw "Building board object failed! Check type for ";
+      }
+      throw "Can't build board object";
+    }
     
     function ConfigNode(n) {
         RED.nodes.createNode(this, n);
@@ -16,13 +30,13 @@ module.exports = function(RED) {
     function DINode(config) {
         RED.nodes.createNode(this, config);
 
-        this.board = RED.nodes.getNode(config.board);
+        this.board = getBoard(RED.nodes.getNode(config.board));
         this.channel = config.channel;
         this.polling = config.polling;
         
         var node = this;
         var intervalFunction = function() {
-          var msg = {payload: 1};
+          var msg = {payload: node.board.DI.getChannel(node.channel)};
           node.send(msg);
         };
         var intervalTimer = setInterval(intervalFunction, this.polling);
@@ -38,11 +52,12 @@ module.exports = function(RED) {
     function DQNode(config) {
         RED.nodes.createNode(this, config);
 
-        this.board = RED.nodes.getNode(config.board);
+        this.board = getBoard(RED.nodes.getNode(config.board));
         this.channel = config.channel;
 
         this.on('input', function (msg) {
-            this.warn("I saw a payload: " + msg.payload + " for " + this.board.name + " channel:" + this.channel);
+            this.board.DQ.setChannel(this.channel, msg.payload);
+            this.warn("I saw a payload: " + msg.payload + " for " + this.board + " channel:" + this.channel);
         });
 
         this.on("close", function() {
